fa92f0f60b51af478c779a819d70c0a464e9ad53
chellem <chellem.void@gmail.com>
2014-07-31 11:57:24 +0400

    add exceptions

---
+++ Exception/NoRegisteredEventException.cs
@@ -0,0 +1,20 @@
+﻿
+namespace EventDispatcher.Exception
+{
+    using System;
+
+    /// <summary>
+    /// Class NoRegisteredEventException
+    /// </summary>
+    class NoRegisteredEventException:Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NoRegisteredEventException"/> class.
+        /// </summary>
+        /// <param name="eventName">Name of the event.</param>
+        public NoRegisteredEventException(string eventName)
+            : base(string.Format("No event \"{0}\" has been registered", eventName))
+        {
+        }
+    }
+}
